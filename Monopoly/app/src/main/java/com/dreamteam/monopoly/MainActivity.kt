5c2911a5e1df1d7f5e47f7754947dd8c5ad17620
AlexanderKuptsov <shon55398@gmail.com>
2018-10-18 02:36:56 +0300

    new GameActivity

--- Monopoly/app/src/main/java/com/dreamteam/monopoly/MainActivity.kt
+++ Monopoly/app/src/main/java/com/dreamteam/monopoly/MainActivity.kt
@@ -2,11 +2,35 @@ package com.dreamteam.monopoly
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.content.Intent
+import android.widget.ImageButton
+import android.content.pm.ActivityInfo
+import android.view.View
 
-class MainActivity : AppCompatActivity() {
+
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    //image button
+    private var buttonPlay: ImageButton? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //setting the orientation to landscape
+        requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE
+
+        //getting the button
+        buttonPlay = findViewById(R.id.buttonPlay)
+
+        //adding a click listener
+        buttonPlay!!.setOnClickListener(this)
+    }
+
+    @Override
+    override fun onClick(v: View) {
+
+        //starting game activity
+        //startActivity(Intent(this, GameActivity.))
     }
 }
--- Monopoly/app/src/main/java/com/dreamteam/monopoly/game/board/cell/GameCellType.kt
+++ Monopoly/app/src/main/java/com/dreamteam/monopoly/game/board/cell/GameCellType.kt
@@ -2,6 +2,6 @@ package com.dreamteam.monopoly.game.board.cell
 
 enum class GameCellType(val type: CompanyType, val cost: Cost) {
 
-    QUARTOS(CompanyType.SOFT, Cost(100, 75, 20)),
+    QUARTUS(CompanyType.SOFT, Cost(100, 75, 20)),
     EDSACK(CompanyType.SOFT, Cost(9999, 9999, 9999))
 }
