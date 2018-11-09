6967645d1dcf1eea720e50330b137b56415938ba
Alex <korneev.ry@yandex.ru>
2018-11-10 01:01:50 +0300

    Mod. creation area

--- app/src/main/java/ru/gdcn/alex/whattodo/creation/ItemsRecyclerAdapter.java
+++ app/src/main/java/ru/gdcn/alex/whattodo/creation/ItemsRecyclerAdapter.java
@@ -1,4 +1,27 @@
 package ru.gdcn.alex.whattodo.creation;
 
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.TextView;
+
+import ru.gdcn.alex.whattodo.R;
+import ru.gdcn.alex.whattodo.objects.Item;
+
 public class ItemsRecyclerAdapter {
+
+
+    class ItemViewHolder extends RecyclerView.ViewHolder {
+
+        ItemViewHolder(@NonNull View itemView) {
+            super(itemView);
+//            headerView = itemView.findViewById(R.id.notes_recyclerview_header);
+        }
+
+        void bind(Item note) {
+
+        }
+
+
+    }
 }
