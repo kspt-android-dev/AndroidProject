fd61ebf9b5d0973c84bce57e2a85ee92fb5f1b83
danila <danilaeremenko@mail.ru>
2018-12-26 03:08:31 +0300

    -all activities and layouts initialized -some code refactoring

--- app/src/main/java/com/example/danila/minerandroid/A_RecordsTableActivity.java
+++ app/src/main/java/com/example/danila/minerandroid/A_RecordsTableActivity.java
@@ -7,5 +7,7 @@ public class A_RecordsTableActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.records_table_layout);
+
     }
 }
