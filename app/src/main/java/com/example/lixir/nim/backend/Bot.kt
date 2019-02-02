fd246b9d69603a9f42c182f575363e109867effc
lixir <pokaprivet10@gmail.com>
2019-02-02 03:40:44 +0300

    Исправлен бот, бот не победим.

--- app/src/main/java/com/example/lixir/nim/backend/Bot.kt
+++ app/src/main/java/com/example/lixir/nim/backend/Bot.kt
@@ -1,13 +1,11 @@
 package com.example.lixir.nim.backend
 
-import kotlin.IllegalArgumentException
-
 class Bot (name: String) : Player(name) {
 
     fun step(list: MutableList<Int>) : Pair<Int, Int>{
         val temp = list.fold(0){total, next -> total xor next}
         val max = list.max() ?: throw IllegalArgumentException()
-        if (temp == 0 || max xor temp > list.indexOf(max)) return Pair(list.indexOf(max), 0)
-        return Pair(list.indexOf(max), max xor temp)
+        list.forEachIndexed { index, i ->  if (i xor temp < i) return Pair(index, i xor temp)}
+        return Pair(list.indexOf(max), 0)
     }
 }
