764a25781f82685ade50479057b2b9b92d6573a8
Malik <hiraev@users.noreply.github.com>
2019-02-03 23:18:19 +0300

    Migration to AndroidX

--- app/src/main/java/org/easyeng/easyeng/MainActivity.java
+++ app/src/main/java/org/easyeng/easyeng/MainActivity.java
@@ -1,8 +1,8 @@
 package org.easyeng.easyeng;
 
-import android.support.design.widget.BottomNavigationView;
-import android.support.v4.app.Fragment;
-import android.support.v7.app.AppCompatActivity;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import androidx.fragment.app.Fragment;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.SparseArray;
 import android.view.Window;
