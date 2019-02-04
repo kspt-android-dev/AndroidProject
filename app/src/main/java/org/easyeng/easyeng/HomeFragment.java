3e8aa6c74de1f32ce2d6e038d81722f8afcb3d40
Malik <hiraev@users.noreply.github.com>
2019-02-05 02:44:45 +0300

    Updates for Home Fragment

--- app/src/main/java/org/easyeng/easyeng/HomeFragment.java
+++ app/src/main/java/org/easyeng/easyeng/HomeFragment.java
@@ -1,17 +1,60 @@
 package org.easyeng.easyeng;
 
 import android.os.Bundle;
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.firebase.ui.firestore.FirestoreRecyclerOptions;
+import com.google.firebase.database.annotations.NotNull;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
 public class HomeFragment extends Fragment {
+
+    private ItemAdapter adapter;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        Log.w("INFO", "onCreateView");
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+        setUpRecyclerView(view);
+        return view;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        adapter.startListening();
+    }
+
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        adapter.stopListening();
+    }
+
+    private void setUpRecyclerView(@NotNull View view) {
+        final FirebaseFirestore firestore = FirebaseFirestore.getInstance();
+        final CollectionReference cardsRef = firestore.collection("Cards");
+        final Query query = cardsRef.orderBy("id", Query.Direction.DESCENDING);
+        final FirestoreRecyclerOptions<Item> options = new FirestoreRecyclerOptions.Builder<Item>()
+                .setQuery(query, Item.class)
+                .build();
+        final RecyclerView recyclerView = view.findViewById(R.id.rv_list);
+        adapter = new ItemAdapter(options);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        recyclerView.setAdapter(adapter);
     }
 }
