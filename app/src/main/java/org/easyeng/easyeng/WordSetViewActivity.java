48f52e4cf7e574839c1837ac15c7e76d9d2ab211
Malik <hiraev@users.noreply.github.com>
2019-02-05 06:34:22 +0300

    Load words and save them onClick

--- app/src/main/java/org/easyeng/easyeng/WordSetViewActivity.java
+++ app/src/main/java/org/easyeng/easyeng/WordSetViewActivity.java
@@ -2,20 +2,50 @@ package org.easyeng.easyeng;
 
 import android.os.Bundle;
 import android.webkit.WebView;
+import android.widget.Button;
 import android.widget.TextView;
+
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import org.easyeng.easyeng.db.AsyncDBManager;
+import org.easyeng.easyeng.db.MyDatabase;
+import org.easyeng.easyeng.db.entities.Word;
+
+import java.util.List;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class WordSetViewActivity extends AppCompatActivity {
     private Item item;
+    private List<Word> words;
+    private MyDatabase myDatabase;
+    private AsyncDBManager asyncDBManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        myDatabase = MyDatabase.getInstance(this, null);
+        asyncDBManager = AsyncDBManager.getInstance(myDatabase);
+
         super.onCreate(savedInstanceState);
         item = getIntent().getParcelableExtra(getString(R.string.cart_extra_name));
         setContentView(R.layout.words_set_activity);
         ((TextView) findViewById(R.id.title)).setText(item.getTitle());
         ((WebView) findViewById(R.id.web_view)).loadData(item.getText(), "text/html", "utf-8");
+
+        final Button button = findViewById(R.id.add_words);
+        FirebaseFirestore.getInstance().collection("Words")
+                .get()
+                .addOnCompleteListener(task -> {
+                    if (task.isSuccessful()) {
+                        QuerySnapshot result = task.getResult();
+                        if (result == null) return;
+                        words = result.toObjects(Word.class);
+                        button.setOnClickListener(v -> {
+                            asyncDBManager.saveWords(words);
+                        });
+                    }
+                });
     }
 
     @Override
