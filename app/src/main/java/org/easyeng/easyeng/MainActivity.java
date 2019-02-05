bf6e77bcbed9704ae6b5a8faef0f06f2c16d23c5
Malik <hiraev@users.noreply.github.com>
2019-02-05 04:38:21 +0300

    Little changes in MainActivity

--- app/src/main/java/org/easyeng/easyeng/MainActivity.java
+++ app/src/main/java/org/easyeng/easyeng/MainActivity.java
@@ -1,19 +1,24 @@
 package org.easyeng.easyeng;
 
-import com.google.android.material.bottomnavigation.BottomNavigationView;
-import androidx.fragment.app.Fragment;
-import androidx.appcompat.app.AppCompatActivity;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.SparseArray;
-import android.widget.Toast;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 public class MainActivity extends AppCompatActivity {
 
     private SparseArray<Fragment> fragmentMap;
+    private SharedPreferences mPrefs;
+    private String CURRENT_FRAGMENT = "current_fragment";
+    private int currentFragment = R.layout.fragment_home;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         fragmentMap = new SparseArray<>(3);
@@ -23,24 +28,36 @@ public class MainActivity extends AppCompatActivity {
 
         final BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation_menu);
 
+        //Get current Screen
+        mPrefs = getSharedPreferences("details", 0);
+        currentFragment = mPrefs.getInt(CURRENT_FRAGMENT, R.layout.fragment_home);
+        //Set selected item in the bottom navigation menu
+        int selectedItemId = R.id.action_home;
+        if (currentFragment == R.layout.fragment_puzzles)
+            selectedItemId = R.id.action_puzzles;
+        else if (currentFragment == R.layout.fragment_words)
+            selectedItemId = R.id.action_words;
+
+        bottomNavigationView.setSelectedItemId(selectedItemId);
         getSupportFragmentManager().beginTransaction()
-                .add(R.id.fragments_container,fragmentMap.get(R.layout.fragment_home))
+                .add(R.id.fragments_container, fragmentMap.get(currentFragment))
                 .commit();
 
         bottomNavigationView.setOnNavigationItemSelectedListener(item -> {
             final int menu_item_id = item.getItemId();
-
-            Toast.makeText(this, String.valueOf(menu_item_id), Toast.LENGTH_LONG).show();
             Fragment fragment = null;
             switch (menu_item_id) {
                 case R.id.action_home:
                     fragment = fragmentMap.get(R.layout.fragment_home);
+                    currentFragment = R.layout.fragment_home;
                     break;
                 case R.id.action_puzzles:
                     fragment = fragmentMap.get(R.layout.fragment_puzzles);
+                    currentFragment = R.layout.fragment_puzzles;
                     break;
                 case R.id.action_words:
                     fragment = fragmentMap.get(R.layout.fragment_words);
+                    currentFragment = R.layout.fragment_words;
                     break;
             }
 
@@ -53,8 +70,17 @@ public class MainActivity extends AppCompatActivity {
         getSupportFragmentManager()
                 .beginTransaction()
                 .setCustomAnimations(R.animator.appearing, R.animator.hiding, R.animator.appearing, R.animator.hiding)
-                .replace(R.id.fragments_container, fragment)
+                .replace(R.id.fragments_container, fragment, fragment.getTag())
                 .commit();
         return true;
+    }
+
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        SharedPreferences.Editor editor = mPrefs.edit();
+        editor.putInt(CURRENT_FRAGMENT, currentFragment);
+        editor.apply();
     }
 }
